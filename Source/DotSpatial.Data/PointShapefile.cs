cf97548187410675ebf03c2ab80d657a2cc73bac
闯 鲁 <156845259@qq.com>
2020-12-24 15:17:45 +0800

    修改GetFeature增加溢出判定

--- Source/DotSpatial.Data/PointShapefile.cs
+++ Source/DotSpatial.Data/PointShapefile.cs
@@ -44,6 +44,10 @@ namespace DotSpatial.Data
             IFeature f;
             if (!IndexMode)
             {
+                if (index < 0 || index >= Features.Count)
+                {
+                    return null;
+                }
                 f = Features[index];
             }
             else
